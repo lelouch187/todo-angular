8a74c9dae28994f272b2ac361bf8b7ba2448145e
lelouch187 <vanushs187@gmail.com>
2024-10-09 21:31:20 +0600

    add themes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {HeaderComponent} from './components/header/header.component';
+import {Theme} from '../types/theme';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    HeaderComponent
+    HeaderComponent,
+    NgClass
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  theme:Theme=Theme.LIGHT;
 
 }
